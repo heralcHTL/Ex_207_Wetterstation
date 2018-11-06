4f618670ff0fe24ef14697c083af30461840d4ab
heralc15\Alex <rednax.herbst@gmail.com>
2018-11-06 10:28:54 +0100

    Exception handling added

--- Ex_207_WeatherStation/src/bl/WeatherStation.java
+++ Ex_207_WeatherStation/src/bl/WeatherStation.java
@@ -10,16 +10,17 @@ package bl;
  * @author Alex
  */
 public class WeatherStation {
+
     private String place;
     private int sealevel;
     private double temperature;
     private int humidity;
 
-    public WeatherStation(String place, int sealevel, double temperature, int humidity) {
+    public WeatherStation(String place, int sealevel, double temperature, int humidity) throws Exception {
         this.place = place;
         this.sealevel = sealevel;
-        this.temperature = temperature;
-        this.humidity = humidity;
+        this.setTemperature(temperature);
+        this.setHumidity(humidity);
     }
 
     public String getPlace() {
@@ -38,20 +39,30 @@ public class WeatherStation {
         this.sealevel = sealevel;
     }
 
-    public double getTemperature() {
+    public double getTemperature() {            // -35 +45
+
         return temperature;
     }
 
-    public void setTemperature(double temperature) {
+    public void setTemperature(double temperature) throws Exception {
+        if (temperature < -35 || temperature > 45)
+        {
+            throw new Exception("Invalid value for Temperature");
+        }
         this.temperature = temperature;
     }
 
-    public int getHumidity() {
+    public int getHumidity() {                   // 0% 100%
+
         return humidity;
     }
 
-    public void setHumidity(int humidity) {
+    public void setHumidity(int humidity) throws Exception {
+        if (humidity < 0 || humidity > 100)
+        {
+            throw new Exception("Invalid value for Humidity");
+        }
         this.humidity = humidity;
     }
-    
+
 }
